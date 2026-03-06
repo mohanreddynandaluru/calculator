7082a67454f65f2bd7ad546b31075946b87887b1
Keshav2102 <kulkarnikeshav.kk@gmail.com>
2026-03-06 15:16:31 +0530

    Refactor addition program into a class method

--- obj/Debug/net8.0/addition.cs
+++ obj/Debug/net8.0/addition.cs
@@ -1,19 +1,13 @@
 using System;
-
-class Program
+ 
+namespace calculator
 {
-    static void Main()
+    public class Addition
     {
-        int num1, num2, sum;
-
-        Console.Write("Enter first number: ");
-        num1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter second number: ");
-        num2 = Convert.ToInt32(Console.ReadLine());
-
-        sum = num1 + num2;
-
-        Console.WriteLine("Sum = " + sum);
+        public void doAddition(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Addition Result: " + result);
+        }
     }
 }
